c8d0e47e3b1aaba3e2f4011999c0f595c304ce03
Xiao Hu <xiaoh@microsoft.com>
2016-07-15 17:05:35 -0700

    Add backend api support

--- server/index.js
+++ server/index.js
@@ -5,15 +5,17 @@ const logger = require('./logger');
 
 const argv = require('minimist')(process.argv.slice(2));
 const setup = require('./middlewares/frontendMiddleware');
+const myApi = require('./middlewares/backendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 
+var app = express();
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+app.use('/api', myApi);
 
 // In production we need to pass these values in instead of relying on webpack
-const app = setup(express(), {
+app = setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/',
 });
